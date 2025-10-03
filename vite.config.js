329c1453466abfe20ec10328571075cac0aa6ab9
aryapromotora-alt <aryapromotora@gmail.com>
2025-10-03 16:38:21 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
@@ -8,6 +10,11 @@ export default defineConfig({
       jsxRuntime: 'automatic',
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
