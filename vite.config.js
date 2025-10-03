c90bb60438e6ccbf7320f5b2399cf591c55fc2e7
aryapromotora-alt <aryapromotora@gmail.com>
2025-10-03 16:17:07 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
   },
 });
